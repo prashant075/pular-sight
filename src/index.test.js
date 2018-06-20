96112eb99b74f20b3a3d80beca846d22784c2c9c
prashant075 <guptaprashant075@gmail.com>
2018-06-20 11:31:13 +0530

    'module9working'

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+var {expect} = require( 'chai');
+var jsdom = require('jsdom');
+var fs = require('fs');
+
+
+describe('Our first test',()=>{
+    it('should be',()=>{
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html',()=>{
+    it('should say',(done)=>{
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function(err, window){
+            const h1 = window.document.getElementByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal("Hello");
+            done();
+            window.close();
+        });
+    });
+});
---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    /*  new webpack.LoaderOptionsPlugin({
+        debug: true,
+        noInfo: false,
+      }), 
+     new webpack.optimize.OccurrenceOrderPlugin(),
+     new webpack.HotModuleReplacementPlugin(),
+     new webpack.NoErrorsPlugin() */
+
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    ]
+  }
+}
